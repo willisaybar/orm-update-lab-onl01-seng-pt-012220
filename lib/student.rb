38a419fad0821b3387e49a39f422e54af459073b
Willis Aybar <willisaybar@gmail.com>
2020-04-09 01:11:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ end
     student
   end
 
-  def self.new_from_db(id, name, grade)
+  def self.new_from_db(row)
       new_student = self.new
       new_student.id = row[0]
       new_student.name = row[1]
