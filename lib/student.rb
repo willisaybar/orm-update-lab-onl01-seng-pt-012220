e4389ee8c80097300e08a44bdb7c04c28d814d61
Willis Aybar <willisaybar@gmail.com>
2020-04-09 00:54:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,11 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+   @id = id
+   @name = name
+   @grade = grade
+ end
 
 
 end
