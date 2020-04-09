d067a0620119ff46fa5a4f637cc4c5ff86749822
Willis Aybar <willisaybar@gmail.com>
2020-04-09 00:55:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,5 +10,17 @@ class Student
    @grade = grade
  end
 
+ def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+
 
 end
